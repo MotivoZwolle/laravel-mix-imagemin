1285fc90b7ba29fee03b16bfc248c3ce33532b46
Ryan Dowling <ryan@ryandowling.me>
2021-03-19 22:31:44 +1100

    fix: incompatability with latest mix

--- src/Imagemin.js
+++ src/Imagemin.js
@@ -14,8 +14,8 @@ class Imagemin {
         return ['copy-webpack-plugin', 'imagemin-webpack-plugin'];
     }
 
-    register(patterns, copyOptions = {}, imageminOptions = {}) {
-        this.patterns = [].concat(patterns);
+    register(copyOptions = {}, imageminOptions = {}) {
+        this.patterns = copyOptions.patterns;
         this.copyOptions = copyOptions;
         this.imageminOptions = Object.assign({
             test: /\.(jpe?g|png|gif|svg)$/i,
@@ -28,7 +28,7 @@ class Imagemin {
         let {patterns, copyOptions, imageminOptions} = this;
 
         return [
-            new CopyWebpackPlugin(patterns, copyOptions),
+            new CopyWebpackPlugin(copyOptions),
             new ImageminPlugin(imageminOptions),
             new ManifestPlugin(patterns),
         ];
